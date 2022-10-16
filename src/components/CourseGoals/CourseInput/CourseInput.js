3b32f2e6819c81783e1951b96085c11c482f832c
Ismail <ismajovic20@gmail.com>
2022-10-16 14:13:58 +0200

    init

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
+//import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
-
+import styles from './CourseInput.module.css';
+/*
 const FormControl = styled.div `
 
   margin: 0.5rem 0;
@@ -35,7 +35,7 @@ const FormControl = styled.div `
 
 
 `;
-
+*/
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
   const [isValid, setIsValed] = useState(true);
@@ -60,10 +60,11 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl invalid={!isValid}>
+      <div className={`${styles} ['form-control] ${!isValid && styles.invalid}`}
+      >
         <label >Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </FormControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
@@ -71,6 +72,14 @@ const CourseInput = props => {
 
 export default CourseInput;
 
+/*
+<FormControl invalid={!isValid}>
+        <label >Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
+      </FormControl>
+
+
+*/
 
 
 
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,7 +1,19 @@
+import React from 'react';
+//import styled from 'styled-components';
+import styles from '../Buttonmodule.css'
+//import '../Buttonmodule.css';
 
-import styled from 'styled-components';
 
-//import '../Button.css';
+
+const Button = props => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
+
+/* 
 
 const Button = styled.button `
   width: 100%
@@ -29,14 +41,6 @@ const Button = styled.button `
   box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
 }
 
-`;
-/*
-const Button = props => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};*/
+`;*/
 
 export default Button;
